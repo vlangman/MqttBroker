c098a35b696e63e487efda1f0f181e2163eeb40c
Vaughan Langman <vlangman00@gmail.com>
2020-04-28 00:32:26 +0200

    removing test insert

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ console.log("Starting broker")
 let broker = new brokerService();
 
 
-broker.handleMessage(JSON.parse('{"msg":"advData","gmac":"D03304003302","obj":[{"dmac":"4105020A33DD","rssi":"-50","data1":"0201061AFF4C0002157777772E6B6B6D636E2E636F6D00000100010001C5"}],"seq":181}')).then().catch(err=>{console.log(err.message)})
+// broker.handleMessage(JSON.parse('{"msg":"advData","gmac":"D03304003302","obj":[{"dmac":"4105020A33DD","rssi":"-50","data1":"0201061AFF4C0002157777772E6B6B6D636E2E636F6D00000100010001C5"}],"seq":181}')).then().catch(err=>{console.log(err.message)})
 let client = broker.getClient();
 
 client.on('connect', function (topic, message) {
