e29dbd04fb9dfedd7ef4d4838005105b964e5962
Vaughan Langman <vlangman00@gmail.com>
2020-04-29 20:04:03 +0200

    Hail mary

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ let brokerService = require("./broker.js")
 let db = require('./database.js')
 db = new db();
 const port = 3000
-const beaconController = require('./controllers/Beacon.controller.js');
 
 
 //init broker service and fetch client
 console.log("Starting broker")
-let broker = new brokerService();
+const broker = new brokerService();
 
 
 // broker.handleMessage(JSON.parse('{"msg":"advData","gmac":"D03304003302","obj":[{"dmac":"4105020A33DD","rssi":"-50","data1":"0201061AFF4C0002157777772E6B6B6D636E2E636F6D00000100010001C5"}],"seq":181}')).then().catch(err=>{console.log(err.message)})
-let client = broker.getClient();
+const client = broker.getClient();
 
 client.on('connect', function (topic, message) {
     console.log("Broker Connected");
